9a8c27d89da121b44ff64c7f8f78503407952d39
Ahmed Muse <amuse@dayliff.com>
2026-03-20 11:19:55 +0300

    feat:added departments and user feedbacks

--- backend/app/api/v1/endpoints/departments.py
+++ backend/app/api/v1/endpoints/departments.py
@@ -1,34 +1,35 @@
+# departments.py
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.db import sessions as deps
 from app.schemas.departments import DepartmentOut, DepartmentCreate, DepartmentUpdate
 from app.crud.crud import department_crud
 
-router = APIRouter()
+router = APIRouter(prefix="/departments", tags=["Departments"])
 
-@router.get("/Read", response_model=list[DepartmentOut])
+@router.get("/", response_model=list[DepartmentOut])
 def departments(skip: int = 0, limit: int = 100, db: Session = Depends(deps.get_db)):
     return department_crud.get_items(db, skip, limit)
 
-@router.post("/Create", response_model=DepartmentOut)
+@router.post("/", response_model=DepartmentOut)
 def create_department(department: DepartmentCreate, db: Session = Depends(deps.get_db)):
     return department_crud.create_item(db, department)
 
-@router.get("/Read/{id}", response_model=DepartmentOut)
+@router.get("/{id}", response_model=DepartmentOut)
 def get_department(id: int, db: Session = Depends(deps.get_db)):
     obj = department_crud.get(db, id)
     if not obj:
         raise HTTPException(404, "Not found")
     return obj
 
-@router.patch("/Update/{id}", response_model=DepartmentOut)
+@router.patch("/{id}", response_model=DepartmentOut)
 def update_department(id: int, data: DepartmentUpdate, db: Session = Depends(deps.get_db)):
     obj = department_crud.get(db, id)
     if not obj:
         raise HTTPException(404, "Not found")
     return department_crud.update_item(db, obj, data)
 
-@router.delete("/Delete/{id}")
+@router.delete("/{id}")
 def delete_department(id: int, db: Session = Depends(deps.get_db)):
     obj = department_crud.delete_item(db, id)
     if not obj:
--- backend/app/routers/user_feedbacks.py
+++ backend/app/routers/user_feedbacks.py
@@ -2,34 +2,33 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.db import sessions as deps
 from app.schemas.departments import DepartmentOut, DepartmentCreate, DepartmentUpdate
-from app.crud.crud import department_crud,user_feedback_crud
+from app.crud.crud import user_feedback_crud
 from app.schemas.schemas import UserFeedbackOut,UserFeedbackCreate,UserFeedbackUpdate
 
-router = APIRouter()
-
-@router.get("/Read", response_model=list[UserFeedbackOut])
+router = APIRouter(prefix="/user/feedbacks", tags=["User Feedbacks"],)
+@router.get("/", response_model=list[UserFeedbackOut])
 def user_feedbacks(skip: int = 0, limit: int = 100, db: Session = Depends(deps.get_db)):
     return user_feedback_crud.get_items(db, skip, limit)
 
-@router.post("/Create", response_model=UserFeedbackCreate)
+@router.post("/", response_model=UserFeedbackCreate)
 def add_user_feedback(user_feedback: UserFeedbackCreate, db: Session = Depends(deps.get_db)):
     return user_feedback_crud.create_item(db, user_feedback)
 
-@router.get("/Read/{id}", response_model=UserFeedbackOut)
+@router.get("/{id}", response_model=UserFeedbackOut)
 def get_user_feedback(id: int, db: Session = Depends(deps.get_db)):
     obj = user_feedback_crud.get(db, id)
     if not obj:
         raise HTTPException(404, "Not found")
     return obj
 
-@router.patch("/Update/{id}", response_model=UserFeedbackOut)
+@router.patch("/{id}", response_model=UserFeedbackOut)
 def update_user_feedback(id: int, data: UserFeedbackUpdate, db: Session = Depends(deps.get_db)):
     obj = user_feedback_crud.get(db, id)
     if not obj:
         raise HTTPException(404, "Not found")
     return user_feedback_crud.update_item(db, obj, data)
 
-@router.delete("/Delete/{id}")
+@router.delete("/{id}")
 def delete_user_feedback(id: int, db: Session = Depends(deps.get_db)):
     obj = user_feedback_crud.delete_item(db, id)
     if not obj:
